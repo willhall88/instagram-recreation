32d6c473cee3a3205b8b7e027539057f4289c3af
willhall88 <willhall88@hotmail.com>
2014-05-27 15:12:37 +0100

    now a post must be created by a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, presence: true
+  validates :username, uniqueness:true, presence: true
 
+  has_many :posts
 end
