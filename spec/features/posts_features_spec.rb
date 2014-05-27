5a1ec8892467fb453cfa2f97b0dd30e969448fd7
Will Hall <willhall88@hotmail.com>
2014-05-27 13:25:00 +0100

    can now add new posts

--- spec/features/posts_features_spec.rb
+++ spec/features/posts_features_spec.rb
@@ -6,4 +6,15 @@ describe 'posts' do
     visit '/posts'
     expect(page).to have_content "no current posts yet"
   end
+
+  it 'should be able to add a new post' do
+    visit '/posts'
+    click_on('New Post')
+
+    expect(current_path).to eq '/posts/new'
+    fill_in 'Caption', with: 'my first post!'
+    click_on('Create Post')
+    expect(current_path).to eq '/posts'
+    expect(page).to have_content 'my first post!'
+  end
 end
