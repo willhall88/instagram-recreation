bf8d6a20f74b3145bb7d953fde5a3529736885b7
Will Hall <willhall88@hotmail.com>
2014-05-27 13:56:35 +0100

    added user sign up and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'posts/index'
+  root to: "posts#index"
 
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
