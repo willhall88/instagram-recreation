08ccaac25c9fdf4e7a096fad0e69df32bccb4cfd
Will Hall <willhall88@hotmail.com>
2014-05-28 11:25:25 +0100

    made the posts and likes dependent on the user and likes dependent onf the post

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,7 +6,7 @@ describe "liking posts" do
   end
 
   specify 'a user cannot like a post more than once' do
-    post = @user.posts.new(:caption => "a new post")
+    post = @user.posts.create(:caption => "a new post")
     like = post.likes.new
     like.user = @user
     expect(like.save).to eq true
@@ -16,7 +16,7 @@ describe "liking posts" do
   end
 
   specify 'a user can like two different posts' do
-    post = @user.posts.new(:caption => "a new post")
+    post = @user.posts.create(:caption => "a new post")
     like = post.likes.new
     like.user = @user
     expect(like.save).to eq true
@@ -27,7 +27,7 @@ describe "liking posts" do
   end
 
   specify 'a user can like two different posts, but not the same post twice' do
-    post = @user.posts.new(:caption => "a new post")
+    post = @user.posts.create(:caption => "a new post")
     like = post.likes.new
     like.user = @user
     expect(like.save).to eq true
@@ -47,7 +47,7 @@ describe "unliking posts" do
   end
 
   specify 'a user cannot like a post more than once' do
-    post = @user.posts.new(:caption => "a new post")
+    post = @user.posts.create(:caption => "a new post")
     like = post.likes.new
     like.user = @user
     expect(like.save).to eq true
@@ -58,5 +58,23 @@ describe "unliking posts" do
     unlike.destroy
     expect(Like.all.count).to eq 0 
   end
+end
 
+
+describe "likes get deleted when a post is deleted" do
+  before do
+    @user = User.create(username:"willhall88", email:"willhall88@hotmail.com", password:'12345678', password_confirmation:'12345678')
+  end
+
+  specify 'a like is deleted when a post is deleted' do
+    post = @user.posts.create(:caption => "a new post")
+    like = post.likes.new
+    like.user = @user
+    like.save
+    expect(Post.all.count).to eq 1
+    expect(Like.all.count).to eq 1
+    post.destroy
+    expect(Post.all.count).to eq 0
+    expect(Like.all.count).to eq 0
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe User do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe "posts and likes get deleted when a user is deleted" do
+  before do
+    @user = User.create(username:"willhall88", email:"willhall88@hotmail.com", password:'12345678', password_confirmation:'12345678')
+  end
+
+  specify 'a post and like is deleted when a user is deleted' do
+    post = @user.posts.create(:caption => "a new post")
+    like = post.likes.new
+    like.user = @user
+    like.save
+    expect(User.all.count).to eq 1
+    expect(Post.all.count).to eq 1
+    expect(Like.all.count).to eq 1
+    @user.destroy
+    expect(User.all.count).to eq 0
+    expect(Post.all.count).to eq 0
+    expect(Like.all.count).to eq 0
+  end
+
+  specify ' a like is deleted from a post owned by a different user, when the first user is deleted' do
+    user2 = User.create(username:"willhall", email:"willhall@hotmail.com", password:'12345678', password_confirmation:'12345678')
+    post = user2.posts.create(:caption => "a new post")
+    like = post.likes.new
+    like.user = @user
+    like.save
+    expect(User.all.count).to eq 2
+    expect(Post.all.count).to eq 1
+    expect(Like.all.count).to eq 1
+    @user.destroy
+    expect(User.all.count).to eq 1
+    expect(Post.all.count).to eq 1
+    expect(Like.all.count).to eq 0
+  end
+end
