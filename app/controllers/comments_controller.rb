fb1a15e6b8cb2d01d88b42ed1275b62dcaf7413b
Will Hall <willhall88@hotmail.com>
2014-06-09 20:26:27 +0100

    changes made on friday, adding ajax comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-     unless params[:comment][:comment].blank?
-      comment = @post.comments.new(params[:comment].permit(:comment))
-      comment.user = current_user
-      comment.save
-      comment.create_hashtags
+    unless params[:comment][:comment].blank?
+      new_comment = @post.comments.new(params[:comment].permit(:comment))
+      new_comment.user = current_user
+      new_comment.save
+      new_comment.create_hashtags
+      WebsocketRails[:comments].trigger 'new', {comment: new_comment, user: new_comment.user}
+      render nothing: true
     end
-    redirect_to '/posts'
   end
 end
